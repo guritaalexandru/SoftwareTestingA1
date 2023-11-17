efd1c2dc1ae1cfef666e2e605ddac2b0882127e7
Victor Delattre <delattrevictor0054@gmail.com>
2023-11-17 23:04:31 +0100

    Add test script for task 4

---
+++ A1_unit_testing_students/test_checkout_and_payment.py
@@ -0,0 +1,103 @@
+import pytest
+from checkout_and_payment import checkoutAndPayment
+from unittest.mock import patch
+
+def test_login_existing_user_correct_wallet():
+    # Test login with correct information
+    pass  # Placeholder for test implementation
+
+def test_login_existing_user_incorrect_wallet():
+    # Test login with incorrect information
+    pass  # Placeholder for test implementation
+
+def test_creating_new_user():
+    # Test creating a new user
+    pass  # Placeholder for test implementation
+
+def test_display_all_products():
+    # Test displaying all available products
+    pass  # Placeholder for test implementation
+
+def test_valid_product_selection():
+    # Test valid product selection
+    pass  # Placeholder for test implementation
+
+def test_invalid_product_number():
+    # Test with an invalid product number
+    pass  # Placeholder for test implementation
+
+def test_non_digit_input():
+    # Test with non-digit input
+    pass  # Placeholder for test implementation
+
+def test_boundary_product_selection():
+    # Test product selection at boundaries
+    pass  # Placeholder for test implementation
+
+def test_out_of_stock_product():
+    # Test selecting an out-of-stock product
+    pass  # Placeholder for test implementation
+
+def test_cart_check_without_items():
+    # Test cart check without adding items
+    pass  # Placeholder for test implementation
+
+def test_cart_check_with_items():
+    # Test cart check after adding items
+    pass  # Placeholder for test implementation
+
+def test_complete_purchase():
+    # Test completing a purchase
+    pass  # Placeholder for test implementation
+
+def test_cancel_purchase():
+    # Test canceling a purchase
+    pass  # Placeholder for test implementation
+
+def test_logout_without_purchase():
+    # Test logout without making a purchase
+    pass  # Placeholder for test implementation
+
+def test_logout_after_purchase():
+    # Test logout after making a purchase
+    pass  # Placeholder for test implementation
+
+def test_cancel_logout():
+    # Test canceling logout
+    pass  # Placeholder for test implementation
+
+def test_add_multiple_items_to_cart():
+    # Test adding multiple items to the cart
+    pass  # Placeholder for test implementation
+
+def test_add_same_item_multiple_times_to_cart():
+    # Test adding the same item multiple times to the cart
+    pass  # Placeholder for test implementation
+
+def test_stock_update_after_purchase():
+    # Test stock update after a purchase
+    pass  # Placeholder for test implementation
+
+def test_user_wallet_update_after_purchase():
+    # Test user wallet update after a purchase
+    pass  # Placeholder for test implementation
+
+def test_continuous_invalid_inputs():
+    # Test continuous invalid inputs
+    pass  # Placeholder for test implementation
+
+def test_large_numbers_for_product_selection():
+    # Test with unusually large numbers for product selection
+    pass  # Placeholder for test implementation
+
+def test_special_characters_or_strings():
+    # Test with special characters or strings as input
+    pass  # Placeholder for test implementation
+
+def test_performance_under_load():
+    # Test performance under load
+    pass  # Placeholder for test implementation
+
+def test_data_persistence():
+    # Test data persistence
+    pass  # Placeholder for test implementation
