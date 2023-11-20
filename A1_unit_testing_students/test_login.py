5e775a1d945bae6c10a77ebbe1bcc98cab6f870e
guritaalexandru <a_gurita@yahoo.com>
2023-11-20 18:36:13 +0200

    Task 1

--- A1_unit_testing_students/test_login.py
+++ A1_unit_testing_students/test_login.py
@@ -4,21 +4,79 @@ from unittest.mock import patch
 import pytest
 from login import login, check_password
 
+valid_test_inputs = ["Ramanathan", "Notaproblem23*"]
+invalid_test_inputs = ["Ramanathan", "Notaproblem23*!"]
+
+new_user_N_inputs = ["NewUser", "", "N"]
+new_user_X_inputs = ["NewUser", "", "X"]
+
+new_user_valid_password = ["NewUser", "", "Y", "ValidPassword1!"]
+new_user_invalid_password = ["NewUser", "", "Y", "invalidpassword"]
+
+
+@pytest.fixture
+def write_to_file_stub(mocker):
+    return mocker.patch('login.write_to_file', return_value=None)
+
+
+@pytest.fixture
+def check_password_stub_incorrect(mocker):
+    return mocker.patch('login.check_password', return_value=False)
+
+
+@pytest.fixture
+def check_password_stub_correct(mocker):
+    return mocker.patch('login.check_password', return_value=True)
+
 
 def test_login_existing_user_correct_password():
-    inputs = ["Ramanathan", "Notaproblem23*"]
-    with patch('builtins.input', side_effect=inputs):
-        assert login() == {"username": "Ramanathan", "wallet": 100}
+    with patch('builtins.input', side_effect=valid_test_inputs):
+        assert login() == {"username": valid_test_inputs[0], "wallet": 100}
 
 
 def test_login_existing_user_incorrect_password():
-    inputs = ["Ramanathan", "Notaproblem23*!"]
-    with patch('builtins.input', side_effect=inputs):
-        assert login() == None
+    with patch('builtins.input', side_effect=invalid_test_inputs):
+        assert login() is None
 
 
-# Test for password validation
-def test_check_password():
-    assert check_password("Password1!") == True
-    assert check_password("noSpecialChar1") == False
-    assert check_password("No1!") == False
+def test_login_new_user_no_register():
+    with patch('builtins.input', side_effect=new_user_N_inputs):
+        assert login() is None
+
+
+def test_login_new_user_X_register():
+    with patch('builtins.input', side_effect=new_user_X_inputs):
+        assert login() is None
+
+
+def test_login_new_user_register_valid_password(write_to_file_stub, check_password_stub_correct):
+    with patch('builtins.input', side_effect=new_user_valid_password):
+        assert login() == {"username": new_user_valid_password[0], "wallet": 0}
+        write_to_file_stub.assert_called_once()
+        check_password_stub_correct.assert_called_once_with(new_user_valid_password[3])
+
+
+def test_login_new_user_register_invalid_password(check_password_stub_incorrect):
+    with patch('builtins.input', side_effect=new_user_invalid_password):
+        assert login() is None
+        check_password_stub_incorrect.assert_called_once_with(new_user_invalid_password[3])
+
+
+def test_check_password_no_capital():
+    assert check_password("password1!") is False
+
+
+def test_check_password_no_numeral():
+    assert check_password("Password!") is False
+
+
+def test_check_password_too_short():
+    assert check_password("Pass1!") is False
+
+
+def test_check_password_no_special_char():
+    assert check_password("Password1") is False
+
+
+def test_check_password_valid():
+    assert check_password("Password1!") is True
